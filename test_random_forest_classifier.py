96aef5cdf5f0a2313a47c52fb1560078272b94f7
ulrichsene <evaulrichsen0@gmail.com>
2024-11-24 12:53:09 -0800

    test fix

--- test_random_forest_classifier.py
+++ test_random_forest_classifier.py
@@ -3,27 +3,27 @@ import numpy as np
 from mysklearn.myclassifiers import MyRandomForestClassifier
 from mysklearn import utils
 
-def test_random_forest_classifier_fit():
-    # 14 instance interview dataset (test case 1)
-    # header_interview = ["level", "lang", "tweets", "phd", "interviewed_well"]
-    X_train_interview = [
-        ["Senior", "Java", "no", "no"],
-        ["Senior", "Java", "no", "yes"],
-        ["Mid", "Python", "no", "no"],
-        ["Junior", "Python", "no", "no"],
-        ["Junior", "R", "yes", "no"],
-        ["Junior", "R", "yes", "yes"],
-        ["Mid", "R", "yes", "yes"],
-        ["Senior", "Python", "no", "no"],
-        ["Senior", "R", "yes", "no"],
-        ["Junior", "Python", "yes", "no"],
-        ["Senior", "Python", "yes", "yes"],
-        ["Mid", "Python", "no", "yes"],
-        ["Mid", "Java", "yes", "no"],
-        ["Junior", "Python", "no", "yes"]
-    ]
-    y_train_interview = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
+# 14 instance interview dataset (test case 1)
+# header_interview = ["level", "lang", "tweets", "phd", "interviewed_well"]
+X_train_interview = [
+    ["Senior", "Java", "no", "no"],
+    ["Senior", "Java", "no", "yes"],
+    ["Mid", "Python", "no", "no"],
+    ["Junior", "Python", "no", "no"],
+    ["Junior", "R", "yes", "no"],
+    ["Junior", "R", "yes", "yes"],
+    ["Mid", "R", "yes", "yes"],
+    ["Senior", "Python", "no", "no"],
+    ["Senior", "R", "yes", "no"],
+    ["Junior", "Python", "yes", "no"],
+    ["Senior", "Python", "yes", "yes"],
+    ["Mid", "Python", "no", "yes"],
+    ["Mid", "Java", "yes", "no"],
+    ["Junior", "Python", "no", "yes"]
+]
+y_train_interview = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
 
+def test_random_forest_classifier_fit():
     model = MyRandomForestClassifier(N=5, M=3, F=2)
     model.fit(X_train_interview, y_train_interview)
 
@@ -34,24 +34,6 @@ def test_random_forest_classifier_fit():
     # assert that each tree used a different subset of features (F)
 
 def test_random_forest_classifier_predict():
-    X_train_interview = [
-        ["Senior", "Java", "no", "no"],
-        ["Senior", "Java", "no", "yes"],
-        ["Mid", "Python", "no", "no"],
-        ["Junior", "Python", "no", "no"],
-        ["Junior", "R", "yes", "no"],
-        ["Junior", "R", "yes", "yes"],
-        ["Mid", "R", "yes", "yes"],
-        ["Senior", "Python", "no", "no"],
-        ["Senior", "R", "yes", "no"],
-        ["Junior", "Python", "yes", "no"],
-        ["Senior", "Python", "yes", "yes"],
-        ["Mid", "Python", "no", "yes"],
-        ["Mid", "Java", "yes", "no"],
-        ["Junior", "Python", "no", "yes"]
-    ]
-    y_train_interview = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
-
     model = MyRandomForestClassifier(N =5, M =3, F = 2)
     model.fit(X_train_interview, y_train_interview)
 
